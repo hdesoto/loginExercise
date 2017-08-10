82d2da39231e5f158bbe2d2168946095e395f462
Hernan de Soto <hdesoto@gmail.com>
2017-08-10 17:14:06 +0200

    Second commit

--- app.js
+++ app.js
@@ -1,43 +1,83 @@
+// REQUIRED
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
-const session = require('express-session')
-const app = express()
+// const session = require('express-session')
+// const moment = require('moment')
+const cookieSession = require('cookie-session')
+const fs = require('fs')
+// const router = express.Router()
+// VARIABLES
 const PORT = 3003
-const pathPublic = path.join(__dirname,'public')
+const pathPublic = path.join(__dirname, 'public')
+
+const app = express()
 
 app.use(express.static(pathPublic))
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
+app.use(cookieSession({
+  name: 'This is a Session Cookie',
+  keys: ['ThisIsTheFirstKey', 'ThisIsASecondKey']
+}))
 
-app.set('view engine','pug')
+// app.use( (req,res,next) =>{
+//   req.session.visits = ++req.session.visits || 1
+//   next()
+// })
 
+app.set('view engine', 'pug')
 
+app.get('/', (req, res) => {
+  if (req.session.logged === false) {
+    res.redirect('/welcome')
+  } else {
+    res.render('pages/login')
+  }
+})
 
-app.get('/',(req,res) => {
+app.get('/login', (req, res) => {
   res.render('pages/login')
 })
 
+app.post('/login', (req, res) => {
+  // LISTADO DE USUARIO REGISTRADOS
+  const aUsers = fs.readFileSync('Usernames-Passwords.txt', 'utf-8').trim().split('\r\n')
+  const user = req.body.user
+  const password = req.body.password
+  const userPassword = user + ':' + password
+  
+  // Intentar hacerlo con una promesa, de manera asíncrona, usando algún módulo NPM que maneje Promises
 
-app.get('/login',(req,res) => {
-  res.render('pages/login')
+  if (aUsers.some(function (e) {
+    return e === userPassword
+  })) {
+    req.session.logged = true
+    res.redirect('/welcome')
+  } else {
+    req.session.logged = false
+    res.redirect('/error')
+  }
 })
 
-
-app.get('/welcome',(req,res) =>{
-  res.render('pages/welcome')
+app.get('/logout', (req, res) => {
+  req.session.logged = false
+  res.redirect('/')
 })
 
+app.get('/welcome', (req, res) => {
+  if (req.session.logged === true) {
+    res.render('pages/welcome', {userLogged: req.session.user})
+  } else {
+    res.render('pages/error')
+  }
+})
+
+app.get('/error', (req, res) => {
+  res.render('pages/error')
+})
 
 app.listen(PORT)
 console.log(`Listening on PORT ${PORT}`)
-
-
-
-
-
-
-
-
-
-
