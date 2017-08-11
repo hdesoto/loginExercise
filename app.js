c3f5ada3996debe1b2bd915450d280866fb34808
Hernan de Soto <hdesoto@gmail.com>
2017-08-11 09:35:50 +0200

    Before adding registration and

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const pathPublic = path.join(__dirname, 'public')
 const app = express()
 
 app.use(express.static(pathPublic))
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -23,31 +22,47 @@ app.use(cookieSession({
   keys: ['ThisIsTheFirstKey', 'ThisIsASecondKey']
 }))
 
-// app.use( (req,res,next) =>{
-//   req.session.visits = ++req.session.visits || 1
-//   next()
-// })
-
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  if (req.session.logged === false) {
+  if (req.session.logged === true) {
+    res.redirect('/welcome')
+  } else {
+    res.redirect('/login')
+  }
+})
+
+app.get('/register', (req,res) => {
+  if(!req.session.logged){
+    res.render('pages/register')
+  } else {
+    //alert('Your are already registered')
+    res.redirect('/')
+  }
+})
+
+app.post('/register', (req,res) =>{
+  const {newUser,newPassword} = req.body
+  
+
+  fs.append('Usernames-Passwords.txt',)
+})
+
+app.get('/login', (req, res) => {
+  if (req.session.logged === true) {
     res.redirect('/welcome')
   } else {
     res.render('pages/login')
   }
 })
 
-app.get('/login', (req, res) => {
-  res.render('pages/login')
-})
-
 app.post('/login', (req, res) => {
   // LISTADO DE USUARIO REGISTRADOS
   const aUsers = fs.readFileSync('Usernames-Passwords.txt', 'utf-8').trim().split('\r\n')
-  const user = req.body.user
-  const password = req.body.password
-  const userPassword = user + ':' + password
+  const {user,password} = req.body
+  // const user = req.body.user
+  // const password = req.body.password
+  const userPassword = `${user}:${password}`
   
   // Intentar hacerlo con una promesa, de manera asíncrona, usando algún módulo NPM que maneje Promises
 
@@ -55,6 +70,7 @@ app.post('/login', (req, res) => {
     return e === userPassword
   })) {
     req.session.logged = true
+    req.session.user = user
     res.redirect('/welcome')
   } else {
     req.session.logged = false
@@ -63,13 +79,16 @@ app.post('/login', (req, res) => {
 })
 
 app.get('/logout', (req, res) => {
+  console.log(req.session)
   req.session.logged = false
   res.redirect('/')
 })
 
 app.get('/welcome', (req, res) => {
   if (req.session.logged === true) {
-    res.render('pages/welcome', {userLogged: req.session.user})
+    res.render('pages/welcome', {
+      user: req.session.user
+    })
   } else {
     res.render('pages/error')
   }
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,9 @@
 /* MAIN JAVASCRIPT TO HANDLE HTML INTERACTION MAINLY WITH jQUERY */
 
-$('#logout').on('click',function(e){
-  console.log('LOGOUT clicked')
-  const url = '/logout'
-  const method = 'POST'
-  $.ajax({url, method})
-    // .then(console.log('Logged out'))
-})
+// $('#logout').on('click',function(e){
+//   console.log('LOGOUT clicked')
+//   const url = '/logout'
+//   const method = 'POST'
+//   $.ajax({url, method})
+//     // .then(console.log('Logged out'))
+// })
